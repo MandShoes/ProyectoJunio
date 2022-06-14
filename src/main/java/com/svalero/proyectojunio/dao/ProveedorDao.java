74b8d437bb652e5809c347e888889d23ea7708ec
AMartinezSValero <a26335@svalero.com>
2022-06-14 20:01:34 +0200

    Clase Proveedor DAO

--- src/main/java/com/svalero/proyectojunio/dao/ProveedorDao.java
+++ src/main/java/com/svalero/proyectojunio/dao/ProveedorDao.java
@@ -1,5 +1,43 @@
 package com.svalero.proyectojunio.dao;
 
+import com.svalero.proyectojunio.domain.Marca;
+import com.svalero.proyectojunio.domain.Proveedor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class ProveedorDao {
-    // TODO: Rellenar Dao
+
+    private Connection connection;
+
+    public ProveedorDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Optional<Proveedor> findById(int id) throws SQLException {
+        String sql = "SELECT * FROM PROVEEDORES WHERE id_proveedor = ?";
+        Proveedor proveedor = null;
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            proveedor = fromResultSet(resultSet);
+        }
+
+        return Optional.ofNullable(proveedor);
+    }
+    private Proveedor fromResultSet(ResultSet resultSet) throws SQLException {
+        Proveedor proveedor = new Proveedor();
+        proveedor.setIdProveedor(resultSet.getInt("id_proveedor"));
+        proveedor.setNombre(resultSet.getString("nombre"));
+        proveedor.setDireccion(resultSet.getString("direccion"));
+        proveedor.setEmail(resultSet.getString("email"));
+        proveedor.setCif(resultSet.getString("cif"));
+        proveedor.setPais(resultSet.getString("pais"));
+        return proveedor;
+    }
 }
