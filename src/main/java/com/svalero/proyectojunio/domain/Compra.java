f91aa88e97382a7914af287aea49c3e1ded83c07
AMartinezSValero <a26335@svalero.com>
2022-06-14 19:51:44 +0200

    Clase DAO de Marca cambiada y nuevos atributos en valoracion y compra

--- src/main/java/com/svalero/proyectojunio/domain/Compra.java
+++ src/main/java/com/svalero/proyectojunio/domain/Compra.java
@@ -8,12 +8,18 @@ public class Compra {
     private LocalDate fechaCompra;
     private boolean pagado;
 
+    private Zapato zapato;
+
+    private Usuario usuario;
+
     public Compra() {
     }
 
-    public Compra(LocalDate fechaCompra, boolean pagado) {
+    public Compra(LocalDate fechaCompra, boolean pagado, Zapato zapato, Usuario usuario) {
         this.fechaCompra = fechaCompra;
         this.pagado = pagado;
+        this.zapato = zapato;
+        this.usuario = usuario;
     }
 
     public void setFechaCompra(LocalDate fechaCompra) {
@@ -22,6 +28,22 @@ public class Compra {
 
     public void setPagado(boolean pagado) {
         this.pagado = pagado;
+    }
+
+    public Zapato getZapato() {
+        return zapato;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setZapato(Zapato zapato) {
+        this.zapato = zapato;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     public LocalDate getFechaCompra() {
